7a6738efef94f3084b069856da187152b620baa4
Guille Tagliavini <guillermina.tagliavini@gmail.com>
2025-08-18 18:21:20 -0300

    Update settings.py
    cambio

--- mi_aplicacion/just_a_student/settings.py
+++ mi_aplicacion/just_a_student/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ag=xlkwn73mqn%znov*kz5&znstq32tiuz=5$ztyi%ol#de$my'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
