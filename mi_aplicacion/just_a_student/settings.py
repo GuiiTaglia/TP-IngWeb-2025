1cad423b6b2caaa3c8faec77eb4ab9fe66ea0cd0
Guille Tagliavini <guillermina.tagliavini@gmail.com>
2025-08-18 19:22:24 -0300

    changes

--- mi_aplicacion/just_a_student/settings.py
+++ mi_aplicacion/just_a_student/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'just_a_student.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    #'default': {
+       # 'ENGINE': 'django.db.backends.sqlite3',
+       # 'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"
+    )
 }
 
 
