a68063d3ff65c4c580e4dd047c431f07b49a15b6
Guille Tagliavini <guillermina.tagliavini@gmail.com>
2025-08-18 19:42:47 -0300

    update settings

--- mi_aplicacion/just_a_student/settings.py
+++ mi_aplicacion/just_a_student/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import os
+import dj_database_url 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,16 +77,22 @@ WSGI_APPLICATION = 'just_a_student.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+
+
+
+#DATABASES = {
     #'default': {
        # 'ENGINE': 'django.db.backends.sqlite3',
        # 'NAME': BASE_DIR / 'db.sqlite3',
     #}
+#}
 
+DATABASES = {
     'default': dj_database_url.config(
         default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"
     )
 }
+
 
 
 # Password validation
@@ -132,8 +141,6 @@ LOGIN_REDIRECT_URL = 'pagina_secundaria'
 LOGOUT_REDIRECT_URL = 'inicio'
 
 # code needed to deploy in Render.com:
-import os
-import dj_database_url
 
 if 'RENDER' in os.environ:
     print("USING RENDER.COM SETTINGS!")
