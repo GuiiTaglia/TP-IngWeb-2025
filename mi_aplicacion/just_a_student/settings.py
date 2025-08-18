83471d19afe97a0cf0021ea9d16a8f21cf206214
Guille Tagliavini <guillermina.tagliavini@gmail.com>
2025-08-18 16:53:34 -0300

    cambios

--- mi_aplicacion/just_a_student/settings.py
+++ mi_aplicacion/just_a_student/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-import os
-import dj_database_url
-from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -144,6 +140,3 @@ if 'RENDER' in os.environ:
                       'whitenoise.middleware.WhiteNoiseMiddleware')
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-STATIC_URL = '/static/'
-
