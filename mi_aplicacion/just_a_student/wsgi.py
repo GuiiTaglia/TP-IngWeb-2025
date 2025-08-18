592c595727c13c73a2c0c8f41155dcff294c6818
Guille Tagliavini <guillermina.tagliavini@gmail.com>
2025-08-18 18:16:15 -0300

    cambios

--- mi_aplicacion/just_a_student/wsgi.py
+++ mi_aplicacion/just_a_student/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mi_aplicacion.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'just_a_student.settings')
 
 application = get_wsgi_application()
