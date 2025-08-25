9438c8503a50fc10068517ed769eadbee1b39865
LuisiCra <craveroluisii@gmail.com>
2025-08-25 18:54:19 -0300

    .

--- mi_aplicacion/bullet_journal/urls.py
+++ mi_aplicacion/bullet_journal/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views 
 from .views.journal_views import journal_list, journal_detail, journal_create, journal_edit
 
-urlpatterns = [
-    path('journals/', views.journal_list, name='journal_list'),
-    path('journals/create/', views.journal_create, name='journal_create'),
-    path('journals/<int:pk>/', views.journal_detail, name='journal_detail'),
-    path('journals/<int:pk>/edit/', views.journal_edit, name='journal_edit'),
-]
+path('journals/', journal_list, name='journal_list'),
+path('journals/<int:id>/', journal_detail, name='journal_detail'),
+path('journals/create/', journal_create, name='journal_create'),
+path('journals/<int:id>/edit/', journal_edit, name='journal_edit'),
