20d99b6dc4974654e3afe0145bcaf619b4e3cd93
Guille Tagliavini <guillermina.tagliavini@gmail.com>
2025-09-10 17:10:22 -0300

    avance validación para registrarse

--- mi_aplicacion/users/urls.py
+++ mi_aplicacion/users/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
-    path('registrer/', views.registro, name='registro'),
+    path('register/', views.registro, name='registro'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- mi_aplicacion/users/views.py
+++ mi_aplicacion/users/views.py
@@ -3,11 +3,52 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from bullet_journal.views.journal_views import home
+from .forms import CustomUserCreationForm
+from django.core.mail import send_mail
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
 
 def inicio(request):
     return render(request, 'inicio.html')
 
 def registro(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False  
+            user.save()
+
+            current_site = get_current_site(request)
+            subject = 'Activa tu cuenta'
+            message = render_to_string('activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            send_mail(subject, message, 'noreply@tusitio.com', [user.email])
+
+            return redirect('check_email')  
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+#def registro(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+#def registro(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -16,7 +57,7 @@ def registro(request):
             return redirect('home')
     else:
         form = UserCreationForm()
-    return render(request, 'registro.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
 
 @login_required
 def logout(request):
