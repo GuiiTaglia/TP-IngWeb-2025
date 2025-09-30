d8fa6e528026e678d43b76e81a8cbc0452ac91ae
LuisiCra <craveroluisii@gmail.com>
2025-09-30 13:55:41 -0300

    correcciones

--- mi_aplicacion/users/views.py
+++ mi_aplicacion/users/views.py
@@ -15,7 +15,7 @@ from django.contrib.auth import logout as auth_logout
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth import get_user_model, login
-
+from django.conf import settings
 
 
 """def inicio(request):
@@ -52,7 +52,7 @@ def registro(request):
             send_mail(
                 'Activa tu cuenta',
                 message,
-                None, 
+                settings.DEFAULT_FROM_EMAIL, 
                 [user.email],
                 fail_silently=False,
             )
