b3d387ae15cdf59ec5f809a02d07bc31d9fabed0
Guille Tagliavini <guillermina.tagliavini@gmail.com>
2025-10-09 16:53:06 -0300

    cambios en el build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,20 @@
 set -o errexit
 
 # install project dependencies
-uv sync
+#uv sync
 
 # make sure django has all the things it needs to run
-cd $(dirname $(find . | grep manage.py$))
-uv run ./manage.py collectstatic --no-input
-uv run ./manage.py migrate
-uv run ./manage.py createsuperuser --username admin --email "guillermina.tagliavini@gmail.com" --noinput || true    
+#cd $(dirname $(find . | grep manage.py$))
+#uv run ./manage.py collectstatic --no-input
+# uv run ./manage.py migrate
+# uv run ./manage.py createsuperuser --username admin --email "guillermina.tagliavini@gmail.com" --noinput || true    
+
+
+
+pip install -r requirements.txt
+
+python manage.py migrate
+python manage.py collectstatic --no-input
+
+# Crea el superusuario (opcional, pero buena práctica)
+python manage.py createsuperuser --username admin --email "guillermina.tagliavini@gmail.com" --noinput || true
