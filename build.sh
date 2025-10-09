f525ecb7c3c5a14ee8b042e9cde79b087eeb5ebe
Guille Tagliavini <guillermina.tagliavini@gmail.com>
2025-10-09 18:14:56 -0300

    ups

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ set -o errexit
 # uv run ./manage.py migrate
 # uv run ./manage.py createsuperuser --username admin --email "guillermina.tagliavini@gmail.com" --noinput || true    
 
-pip unistall Django -y || true
+pip uninstall Django -y || true
 
 pip install -r requirements.txt
 
